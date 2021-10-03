569bc7fbd61c6a0b07af6394cf52313704e119c8
Gyana Madhavan <54224713+Madhavan005@users.noreply.github.com>
2021-10-03 18:47:03 +0530

    Update navabar.php

--- navabar.php
+++ navabar.php
@@ -1,28 +1,33 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-transparent  mb-5">
-      <div class="container-fluid">
-        <a class="navbar-brand" style="font-size: 40px;color: #004d7a;" href="index.php">
-        <i class="fas fa-university"></i> SPARK BANK</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon "></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+  <div class="container-fluid">
+    <a class="navbar-brand" style="font-size: 40px;color: #004d7a;" href="index.php">
+      <i class="fas fa-university"></i> 
+      SPARK BANK
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon "></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li>
+          <a class="nav-link hover-underline-animation " aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link hover-underline-animation" href="viewcustomer.php">View Customer</a>
+        </li>
 
-              <a class="nav-link hover-underline-animation " aria-current="page" href="index.php">Home</a>
-            
-            <li class="nav-item">
-              <a class="nav-link hover-underline-animation" href="viewcustomer.php">View Customer</a>
-            </li>
+        <!-- <li class="nav-item">
+        <a class="nav-link" href="transfermoney.php">Transfer Money</a>
+        </li> -->
 
-            <!-- <li class="nav-item">
-              <a class="nav-link" href="transfermoney.php">Transfer Money</a>
-            </li> -->
-            <li class="nav-item">
-              <a class="nav-link hover-underline-animation" href="history.php">Transaction History</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link hover-underline-animation" href="index.php#contact">Contact Us</a>
-            </li>
-        </div>
-      </div>
+        <li class="nav-item">
+          <a class="nav-link hover-underline-animation" href="history.php">Transaction History</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link hover-underline-animation" href="index.php#contact">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
